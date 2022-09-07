18fbc79a7b0dd54abba169da566c56c12c71aa9e
ganeshfortude <ganeshnayanajith40@gmail.com>
2022-09-07 18:33:36 +0530

    data modifications structure type

--- operational-bom/src/app/operation-bom-home/sample-styles.ts
+++ operational-bom/src/app/operation-bom-home/sample-styles.ts
@@ -5,25 +5,25 @@ export const sampleStyles = [
       {
         color: 'RED',
         smv: 20,
-        operationType: 'Operation-1',
+        operationType: '1',
         action: 'ADD'
       },
       {
         color: 'GREEN',
         smv: 20,
-        operationType: 'Operation-2',
+        operationType: '2',
         action: 'ADD'
       },
       {
         color: 'BLUE',
         smv: 15,
-        operationType: 'Operation-3',
+        operationType: '3',
         action: 'EDIT'
       },
       {
         color: 'BLACK',
         smv: 15,
-        operationType: 'Operation-4',
+        operationType: '4',
         action: 'ADD'
       },
     ]
